4a078d04864cc3246e6d266b52371917b8e4d746
하워드(Howard/천곤홍) <gh.cheon@pet-friends.co.kr>
2024-01-13 17:24:02 +0900

    first commit: frontend

---
+++ todo-frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import http from "./http/http.ts";
+
+const todos = ref([]);
+
+onMounted(async () => {
+  todos.value = (await http.get("/todos")) ?? [];
+});
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
